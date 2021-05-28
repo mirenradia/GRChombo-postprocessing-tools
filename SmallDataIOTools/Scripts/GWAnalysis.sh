2a09a64157cff55ccb7dbf7af614b3be7762cf8a
Miren Radia <miren_radia@yahoo.co.uk>
2021-05-28 10:38:26 +0200

    Update GWAnalysis.sh for reorganised simulation dirs
    Note that it assumes the parameters are set as follows:
     * output_path = "" (default)
     * data_subpath = "data"

--- SmallDataIOTools/Scripts/GWAnalysis.sh
+++ SmallDataIOTools/Scripts/GWAnalysis.sh
@@ -20,38 +20,41 @@ get_exec_path() {
 GRCHOMBO_POSTPROCESSING_DIR=${GRCHOMBO_POSTPROCESSING_DIR-$HOME/GRChombo-postprocessing-tools};
 
 # Set number of OpenMP threads
-OMP_NUM_THREADS=$(getconf _NPROCESSORS_ONLN);
-export OMP_NUM_THREADS
+#OMP_NUM_THREADS=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}');
+#export OMP_NUM_THREADS
 
 # Find GWEnergyMomentum executable
 GW_EXEC=$(get_exec_path "$GRCHOMBO_POSTPROCESSING_DIR/SmallDataIOTools/GWEnergyMomentum" GWEnergyMomentum);
 
+# Set data subpath
+DATA_SUBPATH=${PWD}/data
+
 # Execute GWEnergyMomentum executable in current directory with no arguments
 echo $GW_EXEC
-$GW_EXEC
+$GW_EXEC -p ${DATA_SUBPATH}/Weyl4_extraction_
 
 # Find IntegrateTime executable
 INTEGRATETIME_EXEC=$(get_exec_path "$GRCHOMBO_POSTPROCESSING_DIR/SmallDataIOTools/IntegrateTime" IntegrateTime);
 
 # Integrate power and momentum fluxes in time to get cumulative radiated quantities
-$INTEGRATETIME_EXEC -o GW_energy.dat GW_power.dat
-$INTEGRATETIME_EXEC -o GW_momentum.dat GW_momentum_flux.dat
-$INTEGRATETIME_EXEC -o GW_angular_momentum.dat GW_angular_momentum_flux.dat
+$INTEGRATETIME_EXEC -o ${DATA_SUBPATH}/GW_energy.dat ${DATA_SUBPATH}/GW_power.dat
+$INTEGRATETIME_EXEC -o ${DATA_SUBPATH}/GW_momentum.dat ${DATA_SUBPATH}/GW_momentum_flux.dat
+$INTEGRATETIME_EXEC -o ${DATA_SUBPATH}/GW_angular_momentum.dat ${DATA_SUBPATH}/GW_angular_momentum_flux.dat
 
 # Add radii comment line to time integrated files
-RADII_LINE_1D=$(sed -n '2p' GW_power.dat)
-RADII_LINE_3D=$(sed -n '2p' GW_momentum_flux.dat)
-sed -i "2i${RADII_LINE_1D}" GW_energy.dat
-sed -i "2i${RADII_LINE_3D}" GW_momentum.dat
-sed -i "2i${RADII_LINE_3D}" GW_angular_momentum.dat
+RADII_LINE_1D=$(sed -n '2p' ${DATA_SUBPATH}/GW_power.dat)
+RADII_LINE_3D=$(sed -n '2p' ${DATA_SUBPATH}/GW_momentum_flux.dat)
+sed -i "2i${RADII_LINE_1D}" ${DATA_SUBPATH}/GW_energy.dat
+sed -i "2i${RADII_LINE_3D}" ${DATA_SUBPATH}/GW_momentum.dat
+sed -i "2i${RADII_LINE_3D}" ${DATA_SUBPATH}/GW_angular_momentum.dat
 
 # Find Norm executable
 NORM_EXEC=$(get_exec_path "$GRCHOMBO_POSTPROCESSING_DIR/SmallDataIOTools/Norm" Norm)
 
 #Take norms of momentum and angular momentum
-$NORM_EXEC -o GW_momentum_norm.dat GW_momentum.dat
-$NORM_EXEC -o GW_angular_momentum_norm.dat GW_angular_momentum.dat
+$NORM_EXEC -o ${DATA_SUBPATH}/GW_momentum_norm.dat ${DATA_SUBPATH}/GW_momentum.dat
+$NORM_EXEC -o ${DATA_SUBPATH}/GW_angular_momentum_norm.dat ${DATA_SUBPATH}/GW_angular_momentum.dat
 
 # Add radii comment lines to normed files
-sed -i "2i${RADII_LINE_1D}" GW_momentum_norm.dat
-sed -i "2i${RADII_LINE_1D}" GW_angular_momentum_norm.dat
+sed -i "2i${RADII_LINE_1D}" ${DATA_SUBPATH}/GW_momentum_norm.dat
+sed -i "2i${RADII_LINE_1D}" ${DATA_SUBPATH}/GW_angular_momentum_norm.dat
